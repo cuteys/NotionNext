939b985df52487258a2fc5f86645bbf606142f9f
Cuteys <117161372+cuteys@users.noreply.github.com>
2025-02-06 08:28:54 +0800

    修改 Heo 主题人性化配置

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -4,12 +4,16 @@ const CONFIG = {
 
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2025-02-05', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://blog.cynn.top/' },
+    { title: '练习时长两年半的信安专业生', url: 'https://blog.cynn.top/' },
+    { title: '什么都学一点', url: 'https://blog.cynn.top/' },
+    { title: '什么都会一点', url: 'https://blog.cynn.top/' },
+    { title: '该学的不学', url: 'https://blog.cynn.top/' },
+    { title: '没用的全会', url: 'https://blog.cynn.top/' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -18,12 +22,12 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '分享技术',
+  HEO_HERO_TITLE_2: '与日常生活',
+  HEO_HERO_TITLE_3: 'BLOG.CYNN.TOP',
+  HEO_HERO_TITLE_4: '这是',
+  HEO_HERO_TITLE_5: '一位不正经的学生建立的小站',
+  HEO_HERO_TITLE_LINK: 'https://blog.cynn.top/',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
@@ -51,10 +55,10 @@ const CONFIG = {
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
+  HEO_INFO_CARD_URL2: 'https://github.com/cuteys',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
-  HEO_INFO_CARD_TEXT3: '了解更多',
+  HEO_INFO_CARD_URL3: 'https://cynn.top/',
+  HEO_INFO_CARD_TEXT3: '个人主页',
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
@@ -116,7 +120,7 @@ const CONFIG = {
     }
   ],
 
-  HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
+  HEO_SOCIAL_CARD: false, // 是否显示右侧，点击加入社群按钮
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
